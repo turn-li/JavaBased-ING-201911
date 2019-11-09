1bda0bbb8255fd371f2f2b94dc677d7891b052f4
turnli <nameiswushi@163.com>
2019-11-09 21:31:51 +0800

    push all file

---
+++ src/com/datatype/DataType.java
@@ -0,0 +1,28 @@
+package com.datatype;
+
+/**
+ * @Author:TurnLi
+ * @Description:数据类型
+ * @CreateDate 18:21 2019/11/9
+ * @Modified by:
+ */
+public class DataType {
+
+    public static void main(String[] args) {
+
+        //数据类型共有两类，1.基础数据类型。2.引用类型
+        //1.基础数据类型
+        byte b = 0;
+        short s = 0;
+        int i = 0;
+        long l = 0L;//整形
+        float f = 0.0F;
+        double d = 0.0;//浮点型
+        char c = 0;
+
+
+
+
+    }
+
+}
